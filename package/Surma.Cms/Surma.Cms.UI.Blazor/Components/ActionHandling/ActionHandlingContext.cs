84c7aefba8572a6016abdd0b5f50946a7b43427f
lsurma <kontakt@lsurma.pl>
2024-10-21 23:40:51 +0200

    action handling wip

---
+++ package/Surma.Cms/Surma.Cms.UI.Blazor/Components/ActionHandling/ActionHandlingContext.cs
@@ -0,0 +1,38 @@
+﻿namespace Surma.Cms.UI.Blazor.Components.ActionHandling;
+
+public class ActionHandlingContext
+{
+    public string ElementDisplayName { get; set; } = "";
+    
+    public ActionsStack ActionsStack { get; set; } = new();
+    
+    public bool AnyActionInProgress => ActionsStack.Any(a => a.InProgress);
+    
+    public Dictionary<string, Func<Task>> Actions { get; set; } = new();
+    
+    protected Func<Task> StateHasChanged { get; set; }
+
+    public void Bind(ActionHandlingPanel actionHandlingPanel)
+    {
+        StateHasChanged = actionHandlingPanel.InvokeStateHasChangedAsync;
+    }
+    
+    public void Change(Action<ActionHandlingContext> setter)
+    {
+        setter(this);
+        StateHasChanged.Invoke();
+    }
+
+}
+
+public class ActionsStack : List<ActionState>
+{
+    
+}
+
+public class ActionState
+{
+    public string Name { get; set; } = "";
+    
+    public bool InProgress { get; set; } = false;
+}
---
+++ package/Surma.Cms/Surma.Cms.UI.Blazor/Components/ActionHandling/ActionPanelContentModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Surma.Cms.UI.Blazor.Components.ActionHandling;
+
+public class ActionHandlingPanelContentModel
+{
+    public Type ComponentType { get; set; }
+    
+    public ActionPanelParametersBag Parameters { get; set; } = new ActionPanelParametersBag();
+
+    protected Func<Task> StateHasChanged { get; set; }
+
+    public void Bind(ActionHandlingPanel actionHandlingPanel)
+    {
+        StateHasChanged = actionHandlingPanel.InvokeStateHasChangedAsync;
+    }
+    
+    public void Change(Func<ActionPanelParametersBag, ActionPanelParametersBag> paramsSetter)
+    {
+        var newParams = paramsSetter(Parameters);
+        Parameters = newParams;
+        StateHasChanged.Invoke();
+    }
+}
+
+public class ActionPanelParametersBag : Dictionary<string, object>
+{
+}
